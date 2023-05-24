3c468960c0a5991bebf9bb470bc062253c4d678e
Michael Fomin <mikhail.fomin20@gmail.com>
2023-05-24 18:59:20 +0300

    Create User and store data in database.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,4 +14,13 @@ class UserController extends Controller
 
         return $user;
     }
+
+    public function store()
+    {
+        return User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+    }
 }
