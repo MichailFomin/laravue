a042c659bed0053eaea44c0ee3dc6687590a4b05
Michael Fomin <mikhail.fomin20@gmail.com>
2023-05-25 13:37:43 +0300

    Form Validation using Vue, VeeValidate and Yup

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,6 +1,8 @@
 <script setup>
 import axios from 'axios';
 import { ref, onMounted, reactive} from 'vue';
+import {Form, Field } from 'vee-validate';
+import * as yup from 'yup';
 
 
 const users = ref([]);
@@ -12,21 +14,19 @@ const getUsers = () => {
   })
 };
 
-const form = reactive({
-  name: '',
-  email: '',
-  password: ''
-});
+const schema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required().min(8),
+})
 
-const createUser = () => {
-  axios.post('/api/users', form)
-  .then((response) => {
-    users.value.push(response.data);
-    form.name = '';
-    form.email = '';
-    form.password = '';
-    $('#createUserModal').modal('hide');
-  });
+const createUser = (values) => {
+  axios.post('/api/users', values)
+      .then((response) => {
+        users.value.push(response.data);
+        $('#createUserModal').modal('hide');
+        useResetForm();
+      });
 };
 
 onMounted(() => {
@@ -96,31 +96,36 @@ onMounted(() => {
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body">
-          <form autocomplete="off">
-            <div class="form-group">
-              <label for="name">Name</label>
-              <input v-model="form.name" type="text" class="form-control " id="name"
-                     aria-describedby="nameHelp" placeholder="Enter full name">
-            </div>
+        <Form @submit="createUser" :validation-schema="schema" v-slot="{ errors }">
+          <div class="modal-body">
+            <form autocomplete="off">
+              <div class="form-group">
+                <label for="name">Name</label>
+                <Field name="name" type="text" class="form-control" :class="{ 'is-invalid':errors.name }" id="name"
+                       aria-describedby="nameHelp" placeholder="Enter full name"/>
+                <span class="invalid-feedback">{{ errors.name }}</span>
+              </div>
+
+              <div class="form-group">
+                <label for="email">Email</label>
+                <Field name="email" type="email" class="form-control" :class="{ 'is-invalid':errors.email }" id="email"
+                       aria-describedby="nameHelp" placeholder="Enter full name"/>
+                <span class="invalid-feedback">{{ errors.email }}</span>
+              </div>
+            </form>
 
             <div class="form-group">
-              <label for="email">Email</label>
-              <input v-model="form.email" type="email" class="form-control " id="email"
-                     aria-describedby="nameHelp" placeholder="Enter full name">
+              <label for="email">Password</label>
+              <Field name="password" type="password" class="form-control" :class="{ 'is-invalid':errors.password }" id="password"
+                     aria-describedby="nameHelp" placeholder="Enter password"/>
+              <span class="invalid-feedback">{{ errors.password }}</span>
             </div>
-          </form>
-
-          <div class="form-group">
-            <label for="email">Password</label>
-            <input v-model="form.password" type="password" class="form-control " id="password"
-                   aria-describedby="nameHelp" placeholder="Enter password">
           </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-          <button @click="createUser" type="button" class="btn btn-primary">Save</button>
-        </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary">Save</button>
+          </div>
+        </Form>
       </div>
     </div>
   </div>
