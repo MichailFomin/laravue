04989828e14394e063ccf3cde6b8a21399271abb
Michael Fomin <mikhail.fomin20@gmail.com>
2023-05-30 00:32:16 +0300

    use PHP 8.1 Enums for creating role type(composer packages not updated to 8.1)

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Enums\RoleType;
 
 class User extends Authenticatable
 {
@@ -49,5 +51,12 @@ class User extends Authenticatable
     public function getFormattedCreatedAtAttribute()
     {
         return $this->created_at->format(config('app.date_format'));
+    }
+
+    public function role(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => RoleType::from($value)->name,
+        );
     }
 }
